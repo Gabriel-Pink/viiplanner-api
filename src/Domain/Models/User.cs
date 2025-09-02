bb87a9a51dcd2a5b33704a49e24f7fb649e90681
Gabriel-Pink <infra@gabriel-rosa.com>
2025-09-02 15:49:38 -0300

    Add User model to define user data structure

---
+++ src/Domain/Models/User.cs
@@ -0,0 +1,12 @@
+namespace Viiplanner.Domain.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public User(string name)
+    {
+        Name = name;
+    }
+}
